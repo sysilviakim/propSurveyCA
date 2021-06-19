423dcf53ceb1c84ebc87c77ae24cd1367e8fbd8a
jennlopez49 <jl0049a@student.american.edu>
2021-06-19 02:03:01 -0700

    new stargazer function to make the shortened version of the table

--- R/05_reg_prelim.R
+++ R/05_reg_prelim.R
@@ -38,23 +38,35 @@ stargazer_custom_tex(model_no_wgt %>% map("prop_16"))
 
 # Model with weights (education is categorical) ================================
 model_weight <- list(
-  null = y %>% map(~ reg_form(.x, "null", survey = TRUE)),
+  # null = y %>% map(~ reg_form(.x, "null", survey = TRUE)),
   demo_geo = y %>% map(~ reg_form(.x, model_nested[1:2], survey = TRUE)),
   demo_geo_party = y %>% map(~ reg_form(.x, model_nested[1:3], survey = TRUE)),
   all = y %>% map(~ reg_form(.x, model_nested, survey = TRUE))
 )
 
 
-# to see the table
+# to see the table from paper 
+
+stargazer_custom_omit(model_weight %>% map("prop_15"), lab = 15)
+stargazer_custom_omit(model_weight %>% map("prop_16"), lab = 16)
+
+# to see full table 
 stargazer_custom(model_weight %>% map("prop_15"), lab = 15)
 stargazer_custom(model_weight %>% map("prop_16"), lab = 16)
 
+## to see odds ratios 
+
 stargazer_custom_odds(model_weight %>% map("prop_15"), lab = 15)
 stargazer_custom_odds(model_weight %>% map("prop_16"), lab = 16)
+
 # to make the latex tables with log odds
 prop15_table <- stargazer_custom_tex(model_weight %>% map("prop_15"), lab = 15)
 
 prop16_table <- stargazer_custom_tex(model_weight %>% map("prop_16"), lab = 16)
+
+
+prop_15_short <- stargazer_tex_omit(model_weight %>% map("prop_15"), lab = 15)
+prop_16_short <- stargazer_tex_omit(model_weight %>% map("prop_16"), lab = 16)
 
 
 # to produce the tables with odd-ratios 
