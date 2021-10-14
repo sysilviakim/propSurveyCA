f4c12e5d70b0c0a870379b0fae10abcc2b2af639
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-10-14 03:10:53 -0400

    use here::here and subfolders...

--- R/margins.R
+++ R/margins.R
@@ -9,54 +9,50 @@ cal_survey$party <- ifelse(
   cal_survey$pid3 != 1 & cal_survey$pid3 != 2, 3, cal_survey$pid3
 )
 
-## regressions - ols and glm
-ols <- lm(prop_15 ~ gender + age + race5 + educ + income3 + ca_region + party,
+# Regressions: ols and glm =====================================================
+ols <- lm(
+  prop_15 ~ gender + age + race5 + educ + income3 + ca_region + party,
   data = cal_survey, weights = weight_ca
 )
-glm <- glm(prop_15 ~ gender + age + race5 + educ + income3 + ca_region + party +
-  elec_int_state + covid_response,
-data = cal_subset, weight = weight_ca, family =
-  "quasibinomial"
+glm <- glm(
+  prop_15 ~ gender + age + race5 + educ + income3 + ca_region + party +
+    elec_int_state + covid_response,
+  data = cal_subset, weight = weight_ca,
+  family = "quasibinomial"
 )
-ols_16 <- lm(prop_16 ~ gender + age + race5 + educ + income3 + ca_region + party,
+ols_16 <- lm(
+  prop_16 ~ gender + age + race5 + educ + income3 + ca_region + party,
   data = cal_survey, weights = weight_ca
 )
-glm_16 <- glm(prop_16 ~ gender + age + race5 + educ + income3 + ca_region + party +
-  elec_int_state + covid_response,
-data = cal_subset, weight = weight_ca, family =
-  "quasibinomial"
+glm_16 <- glm(
+  prop_16 ~ gender + age + race5 + educ + income3 + ca_region + party +
+    elec_int_state + covid_response,
+  data = cal_subset, weight = weight_ca, family = "quasibinomial"
 )
 summary(glm)
-### margins
-m_glm <- margins(glm)
 
+# Margins calculations =========================================================
+m_glm <- margins(glm)
 m_glm_16 <- margins(glm_16)
 
-## comparing
-
-
-stargazer(ols, type = "text", out = "ols.tex")
-stargazer(ols_16, type = "text", out = "ols.tex")
+# Compare and export ===========================================================
+stargazer(ols, type = "text", out = here("tab", "ols.tex"))
+stargazer(ols_16, type = "text", out = here("tab", "ols.tex"))
 
 export_summs(m_glm, type = "text")
-
 export_summs(m_glm_16, type = "text")
 
-### Plots to match the plot from Fisk article
-
+## Plots to match the plot from Fisk article
 glm_15_lat <- margins(glm, variables = "race5")
 
-
-pdf(file = "prop_15_ame.pdf")
+pdf(file = here("fig", "prop_15_ame.pdf"))
 plot(glm_15_lat, xaxt = "n")
 axis(1, at = seq(1, 4, 1), labels = c("Black", "Hispanic", "Asian", "Other"))
 dev.off()
 
-
 glm_16_lat <- margins(glm_16, variables = "race5")
 
-
-pdf(file = "prop_16_ame.pdf")
+pdf(file = here("fig", "prop_16_ame.pdf"))
 plot(glm_16_lat, xaxt = "none")
 axis(1, at = seq(1, 4, 1), labels = c("Black", "Hispanic", "Asian", "Other"))
 dev.off()
