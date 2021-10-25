c0f37a9ad53fc814477630b9919d9c8a381e0f4a
jennlopez49 <jl0049a@student.american.edu>
2021-10-24 20:28:05 -0400

    edited packages, first differences calculation

--- R/first_differences.R
+++ R/first_differences.R
@@ -33,9 +33,36 @@ simprob.A1 <- plogis(betadraw_16 %*% hypo.case_16)
 hypo.case_16["race5Hispanic"] <- 1
 simprob.B1 <- plogis(betadraw_16 %*% hypo.case_16)
 
-simprob.diff0 <- simprob.B1 - simprob.A1
+simprob.diff1 <- simprob.B1 - simprob.A1
 summary(hypo.case_16)
 
+simprob_total <- simprob.diff1 - simprob.diff0
+
+results.A0 <- append(quantile(simprob.A0, probs=c(0.5, 0.025, 0.975)), 
+                     sd(simprob.A0))
+results.B0 <- append(quantile(simprob.B0, probs=c(0.5, 0.025, 0.975)), 
+                     sd(simprob.B0))
+results.diff0 <- append(quantile(simprob.diff0, probs=c(0.5, 0.025, 0.975)), 
+                        sd(simprob.diff0))
+
+results.A1 <- append(quantile(simprob.A1, probs=c(0.5, 0.025, 0.975)), 
+                     sd(simprob.A1))
+results.B1 <- append(quantile(simprob.B1, probs=c(0.5, 0.025, 0.975)), 
+                     sd(simprob.B1))
+results.diff1 <- append(quantile(simprob.diff1, probs=c(0.5, 0.025, 0.975)), 
+                        sd(simprob.diff1))
+
+results.dd <- append(quantile(simprob_total, probs=c(0.5, 0.025, 0.975)), 
+                     sd(simprob_total))
+
+first.differences <- rbind(results.A0, results.B0, results.diff0, results.A1, 
+                           results.B1, results.diff1, results.dd)
+colnames(first.differences) <- c("Median", "2.5%", "97.5%", "(se)")
+rownames(first.differences) <- c("Prop. 15: Non-Latino", "Prop. 15: Latino",
+                                 "Prop. 15 Difference", "Prop. 16: Non-Latino",
+                                 "Prop. 16: Latino", "Prop. 16 Difference",
+                                 "Difference: Prop. 15 and Prop. 16")
+print(round(first.differences, digits=3))
 ###### Using lists
 
 prop_coefs <- list(
